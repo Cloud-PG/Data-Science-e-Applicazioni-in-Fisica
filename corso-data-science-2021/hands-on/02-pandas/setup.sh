4ce7a8c706ed2ea4838e664110ee3045e22eabaa
dciangot <diego.ciangottini@gmail.com>
2021-05-18 11:12:30 +0200

    prima lezione hands-on

---
+++ corso-data-science-2021/hands-on/02-pandas/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+
+if [ -d input ]; then
+    exit 0
+fi
+mkdir input
+
+DATASETS="zynicide/wine-reviews nolanbconaway/pitchfork-data dansbecker/powerlifting-database residentmario/things-on-reddit jpmiller/publicassistance datasnaek/youtube-new"
+
+mkdir -p ~/.kaggle/
+
+echo '{"username":"dciangot","key":"${KAGGLE_KEY}"}' >  ~/.kaggle/kaggle.json
+
+for slug in $DATASETS
+do
+    name=`echo $slug | cut -d '/' -f 2`
+    dest="input/$name"
+    mkdir -p $dest
+    kaggle d download -p $dest --unzip $slug
+done
+
+# Things on reddit is a zip of a zip. Kaggle d --unzip doesn't do a deep unzip, but kernels does
+# unzip -q input/things-on-reddit/top-things.zip -d input/things-on-reddit/top-things
---
+++ corso-data-science-2021/hands-on/README.md
@@ -0,0 +1,4 @@
+
+## Credits 
+
+This material is a smalla adaptation from [Kaggle courses](https://www.kaggle.com/learn). All credits goes to the great work done there.
