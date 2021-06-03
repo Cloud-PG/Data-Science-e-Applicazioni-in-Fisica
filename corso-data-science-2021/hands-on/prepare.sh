8a753d88987ae1cca134835bc2dc98c5bf1d8ba0
dciangot <dciangot@cern.ch>
2021-06-03 10:10:42 +0200

    Create prepare.sh

---
+++ corso-data-science-2021/hands-on/prepare.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+apt install -y git && pip3 install mlxtend==0.18.0 numpy==1.16.2
+
+mkdir -p /workspace && mkdir -p /opt && \
+   cd /opt && git clone https://github.com/Kaggle/learntools.git && \
+   pip3 install -e ./learntools/
